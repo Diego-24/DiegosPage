57886ace71a6a615086c8126ed3edb54dac9eb32
Diego Sanchez <sanchezdiegoamazing@gmail.com>
2015-05-21 17:26:03 -0700

    Added working parallax scrolling

--- index.php
+++ index.php
@@ -2,9 +2,21 @@
 <html>
 <head>
 	<title></title>
+	<link href='http://fonts.googleapis.com/css?family=Warnes' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 <body>
-
+	<div class="parallax-window" data-parallax="scroll" data-image-src="img/color-background.jpg">
+		Diego Sanchez Web Designer
+	</div>
+	<div class="parallax-window" data-parallax="scroll" data-image-src="img/grey-background.jpg">
+		My Projects
+	</div>
+	<div class="parallax-window" data-parallax="scroll" data-image-src="img/tech-background.jpg">
+		Contact Me
+	</div>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	<script src="js/parallax.js"></script>
+	<script type="text/javascript" href="js/main.js"></script>
 </body>
 </html>
