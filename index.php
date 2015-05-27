3d4f7f008d783825e7f75904c1629d9b31f5a0b9
Diego Sanchez <sanchezdiegoamazing@gmail.com>
2015-05-26 18:29:25 -0700

    added carousel

--- index.php
+++ index.php
@@ -2,21 +2,84 @@
 <html>
 <head>
 	<title></title>
-	<link href='http://fonts.googleapis.com/css?family=Warnes' rel='stylesheet' type='text/css'>
+	<link href="css/bootstrap.min.css" rel="stylesheet" />
+	<link href='http://fonts.googleapis.com/css?family=Kelly+Slab' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Lora' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 <body>
 	<div class="parallax-window" data-parallax="scroll" data-image-src="img/color-background.jpg">
-		Diego Sanchez Web Designer
+		Diego Sanchez<br>Web Designer
 	</div>
 	<div class="parallax-window" data-parallax="scroll" data-image-src="img/grey-background.jpg">
 		My Projects
+		<div class="container">
+			<br>
+			<div id="myCarousel" class="carousel slide" data-ride="carousel">
+				<!-- Indicators -->
+				<ol class="carousel-indicators">
+				  <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+				  <li data-target="#myCarousel" data-slide-to="1"></li>
+				  <li data-target="#myCarousel" data-slide-to="2"></li>
+				  <li data-target="#myCarousel" data-slide-to="3"></li>
+				</ol>
+
+				<!-- Wrapper for slides -->
+				<div class="carousel-inner" role="listbox">
+
+				  <div class="item active">
+				    <img src="img/portfolio.png" alt="portfolio" width="460" height="345">
+				    <div class="carousel-caption">
+				      <h1>Portfolio</h1>
+				      <h3>I created a portfolio for my very first project using html.</h3>
+				    </div>
+				  </div>
+
+				  <div class="item">
+				    <img src="img/dorado.png" alt="Dorado" width="460" height="345">
+				    <div class="carousel-caption">
+				      <h1>Business Project</h1>
+				      <h3>I remade a website for DORADO Design & Construction Inc.</h3>
+				    </div>
+				  </div>
+
+				  <div class="item">
+				    <img src="img/project2.png" alt="Dorado" width="460" height="345">
+				    <div class="carousel-caption">
+				      <h1>Business Project</h1>
+				      <h3>Another slide of the Dorado project.</h3>
+				    </div>
+				  </div>
+
+				  <div class="item">
+				    <img src="img/awesomenauts.png" alt="Awesomenauts" width="460" height="345">
+				    <div class="carousel-caption">
+				      <h1>Awesomenauts</h1>
+				      <h3>Using melon.js, I made a version of the game Awesomenauts.</h3>
+				    </div>
+				  </div>
+
+				</div>
+
+				<!-- Left and right controls -->
+				<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+				  <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+				  <span class="sr-only">Previous</span>
+				</a>
+				<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+				  <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+				  <span class="sr-only">Next</span>
+				</a>
+			</div>
+		</div>
+		<br>
 	</div>
 	<div class="parallax-window" data-parallax="scroll" data-image-src="img/tech-background.jpg">
-		Contact Me
+		Contact Me<br>
 	</div>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 	<script src="js/parallax.js"></script>
+	<script src="js/bootstrap.min.js"></script>
 	<script type="text/javascript" href="js/main.js"></script>
 </body>
 </html>
